5d5df7a9a64b50d86acd694dd0bdfc82492d6372
ccfeng <ccfeng@asiaa.sinica.edu.tw>
2012-10-23 09:56:30 +0000

    /hpchome/ccfeng/lustre/m81/basic ^/dev/galaxy_module
    add galaxy intrinsic parameters: density and velocity
    going to merge to trunk
    
    
    
    git-svn-id: file:///home/ccfeng/antares_v0.58@28 901975ac-0c86-48c1-8a46-a989f79c1a91

--- dev/galaxy_module/patch/show.m
+++ dev/galaxy_module/patch/show.m
@@ -1,11 +1,17 @@
 clear all
 close all
-filename='M0008.h5';
+
+for i = 0:length(dir('../*.h5'))-1
+filename=sprintf('../M%04d.h5',i);
 den = hdf5read(filename,'density');
 x = hdf5read(filename,'x');
 y = hdf5read(filename,'y');
 imagesc(x,y,(den'));
+title(sprintf('frame %d',i)) 
 axis xy
 axis equal
 axis tight
 colorbar
+
+pause(0.1);
+end
