1330a86e18878555e9c7c558b9a1b3535b601bfa
ccfeng <ccfeng@asiaa.sinica.edu.tw>
2013-07-04 05:08:43 +0000

    add new antares branch for para0703
    git-svn-id: file:///home/ccfeng/antares_v0.58@249 901975ac-0c86-48c1-8a46-a989f79c1a91

---
+++ branch/para0703Spiral1024Snsd8Gaussian20-6f1SlowSG/src/init2d.f90
@@ -0,0 +1,26 @@
+subroutine init2d(q_loc)
+use common_params
+use GALAXY,only:gasdensity,RC,spiral,InitGasDensity
+implicit none
+double precision::q_loc(1-ibuf:ncell_loc(1)+ibuf,1-jbuf:ncell_loc(2)+jbuf,NVAR)
+integer::i,j
+! implement your initial condition here
+double precision:: r_loc,vx,vy,rho,CV
+double precision:: pspd
+pspd = real(spiral.w)/2.d0
+q_loc = 0.d0
+   do j=1,ncell_loc(2)
+     do i=1,ncell_loc(1)
+          r_loc = dsqrt(x_loc(i)**2+y_loc(j)**2)
+          CV = RC(r_loc) - pspd*r_loc
+          vx = -CV*y_loc(j)/r_loc
+          vy =  CV*x_loc(i)/r_loc
+          rho = gasdensity(r_loc,0.d0)
+          InitGasDensity(i,j) = rho
+          q_loc(i,j,1) = rho
+          q_loc(i,j,2) = vx*rho
+          q_loc(i,j,3) = vy*rho
+     enddo
+   enddo
+
+end subroutine
